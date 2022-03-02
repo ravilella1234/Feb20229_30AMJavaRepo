631ab73998572a2736be4f9a6a4a465ab0d55cd3
ravilella1234 <masters.selenium@gmail.com>
2022-03-02 10:26:11 +0530

    Contructors

---
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/Employee.java
@@ -0,0 +1,39 @@
+package kotak.loans.homeloans;
+
+public class Employee
+{
+	int eno;
+	String ename;
+	
+	public Employee()
+    {
+		System.out.println("iam default constructor....");
+    }
+
+	public Employee(int eno, String ename) 
+	{
+		this.eno = eno;
+		this.ename = ename;
+	}
+
+	public Employee(int eno) 
+	{
+		this.eno = eno;
+	}
+
+	public static void main(String[] args) 
+	{
+		Employee e1 = new Employee();
+		System.out.println(e1.eno);
+		System.out.println(e1.ename);
+		
+		Employee e2 = new Employee(1744,"sai");
+		System.out.println(e2.eno);
+		System.out.println(e2.ename);
+		
+		Employee e3 = new Employee(1745);
+		
+		
+	}
+
+}
---
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/Student.java
@@ -0,0 +1,6 @@
+package kotak.loans.homeloans;
+
+class Student 
+{
+
+}
