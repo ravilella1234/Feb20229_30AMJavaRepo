3b76d7e92b989374d030d4fa0cfaf88bf194c86a
ravilella1234 <masters.selenium@gmail.com>
2022-03-07 10:26:56 +0530

    lopings & decessions & arrays & scanner

---
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/Multiplication.java
@@ -0,0 +1,25 @@
+package kotak.loans.homeloans;
+
+public class Multiplication 
+{
+
+	public static void main(String[] args) 
+	{
+		/*
+		 * int n = 3;
+		 * 
+		 * for(int i=1;i<=10;i++) { System.out.println(n+"*"+i+"="+n*i); }
+		 */
+		
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=1;j<=10;j++)
+			{
+				System.out.print(i+"*"+j+"="+i*j + "  ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
