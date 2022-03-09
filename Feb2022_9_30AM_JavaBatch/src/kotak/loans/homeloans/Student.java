2762608571069fd8904d4921ef03df018e06d3c4
ravilella1234 <masters.selenium@gmail.com>
2022-03-09 10:25:19 +0530

    Two Dimenssion Array & USer Defined Array

--- Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/Student.java
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/Student.java
@@ -2,5 +2,53 @@ package kotak.loans.homeloans;
 
 class Student 
 {
+	int sno;
+	String sname;
+	
+	public Student(int sno, String sname) 
+	{
+		super();
+		this.sno = sno;
+		this.sname = sname;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Student [sno=" + sno + ", sname=" + sname + "]";
+	}
+
+	public static void main(String[] args) 
+	{
+		Student s = new Student(1744, "sai");
+		System.out.println(s);
+		System.out.println(s.toString());
+		
+		//System.out.println(s.sno);
+		//System.out.println(s.sname);
+		
+		
+		Student s1 = new Student(100, "s1");
+		Student s2 = new Student(200, "s2");
+		Student s3 = new Student(300, "s3");
+		Student s4 = new Student(400, "s4");
+		
+		//System.out.println(s1);
+		//System.out.println(s2);
+		//System.out.println(s3);
+		//System.out.println(s4);
+		
+		Student[] st = new Student[4];
+		st[0]=s1;
+		st[1]=s2;
+		st[2]=s3;
+		st[3]=s4;
+		
+		for(Student stu:st)
+		{
+			System.out.println(stu);
+		}
+	}
+	
 
 }
---
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/homeloans/TwoDimensionArray1.java
@@ -0,0 +1,39 @@
+package kotak.loans.homeloans;
+
+public class TwoDimensionArray1 
+{
+
+	public static void main(String[] args)
+	{
+		//int[] a = {10,20,30,40};
+		
+		int[][] a = {
+						{10,20,30},
+						{40,50},
+						{70,80,90,10,20}
+					};
+		
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a[i].length;j++)
+			{
+				System.out.print(a[i][j] + "|");
+			}
+			System.out.println();
+		}
+		
+		
+		System.out.println("--------------------------------------");
+		
+		for(int[] p:a)
+		{
+			for(int q:p)
+			{
+				System.out.print(q + "  ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
