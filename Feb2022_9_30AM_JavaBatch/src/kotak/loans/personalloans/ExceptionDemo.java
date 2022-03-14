2c4574ccae21e4838b81a16f003489383900a4d5
ravilella1234 <masters.selenium@gmail.com>
2022-03-14 10:16:32 +0530

    Exceptionsday-1

---
+++ Feb2022_9_30AM_JavaBatch/src/kotak/loans/personalloans/ExceptionDemo.java
@@ -0,0 +1,34 @@
+package kotak.loans.personalloans;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ExceptionDemo 
+{
+
+	public static void main(String[] args)
+	{
+		int nr,dr,result;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(true) 
+		{
+			System.out.println("Enter Numarator Value :");
+			nr = sc.nextInt();
+			System.out.println("Enter Denominator value :");
+			dr = sc.nextInt();
+			try 
+			{
+				result = nr / dr;
+				System.out.println(result);
+				break;
+			}
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			} 
+		}
+	}
+
+}
